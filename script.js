a6887316c6c571649acb1aa62f7819085c4b1d57
Amanda G <amagonzalez27@gmail.com>
2018-06-10 20:14:02 -0400

    adding starting project

---
+++ script.js
@@ -0,0 +1,52 @@
+var css = document.querySelector('h3');
+var color1 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var body = document.getElementById('gradient');
+var random = document.querySelector('button');
+
+window.onload = function() {
+    setGradient()
+}
+
+function setGradient() {
+    body.style.background =
+        'linear-gradient(to right,'
+        + color1.value
+        + ', '
+        + color2.value
+        + ')';
+        css.textContent = body.style.background + ";";
+}
+
+
+
+color1.addEventListener('input', setGradient);
+color2.addEventListener('input', setGradient);
+
+
+//CONVERT RANDOM RGB TO HEX
+function componentToHex(c) {
+    //turn random number to string
+    var hex = c.toString(16);
+    //if hex.length equals 1 then add 0 to hex otherwise return hex
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    //RGB variables
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+//RANDOM BUTTON
+random.addEventListener('click', function() {
+    //update the value
+    color1.value = rgbToHex();
+    color2.value = rgbToHex();
+    //update the background
+    setGradient();
+});
+
